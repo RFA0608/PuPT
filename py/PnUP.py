b539a4d761a69958ea06094c9e4a44041c514e32
RFA0608 <159212138+RFA0608@users.noreply.github.com>
2025-10-16 05:24:04 +0900

    Add files via upload

--- py/PnUP.py
+++ py/PnUP.py
@@ -10,68 +10,81 @@ import tcp_protocol_server as tcs
 HOST = 'localhost'
 PORT = 9999
 
-Poly_Degree = 2**13
-Plain_Modulus = 24
+class pupt:
+    Poly_Degree = 2**13
+    Plain_Modulus = 24
+    Plain_Modulus_Specify = -1
 
-def launch_pack():
-    time.sleep(0.01)
-    subprocess.run([pack_prog])
+    def __init__(self, poly_d, plain_b):
+        self.Poly_Degree = poly_d
+        self.Plain_Modulus = plain_b
+        self.Plain_Modulus_Specify = -1
 
-def launch_unpack():
-    time.sleep(0.01)
-    subprocess.run([unpack_prog])    
-
-def pack_main(data):
-    packed_data = []
-
-    p = multiprocessing.Process(target=launch_pack)
-    p.start()
-
-    with tcs.tcp_server(HOST, PORT) as tcsp:
-        # send poly_degree and plain_modulus(bits)
-        tcsp.send(Poly_Degree)
-        tcsp.send(Plain_Modulus)
-
-        # send the number of data
-        tcsp.send(len(data))
-        for i in range(len(data)):
-            tcsp.send(f"{data[i]}")
-
-        # receive poly_size and packed ring coefficient
-        _, k = tcsp.recv()
-        for i in range(k):
-            _, recv_data = tcsp.recv()
-            packed_data.append(int(recv_data))
-
-    p.join()
-
-    return packed_data
-
-def unpack_main(packed_data, datalen):
-    data = []
-
-    up = multiprocessing.Process(target=launch_unpack)
-    up.start()
-
-    with tcs.tcp_server(HOST, PORT) as tcsp:
-        # send poly_degree and plain_modulus(bits)
-        tcsp.send(Poly_Degree)
-        tcsp.send(Plain_Modulus)
-
-        # send the number of data
-        k = datalen
-        tcsp.send(k)
-
-        # send the number of packed_data
-        for i in range(len(packed_data)):
-            tcsp.send(f"{packed_data[i]}")
-
-        # receive data
-        recv_unpacked_data = -1
-        for i in range(k):
-            _, recv_unpacked_data = tcsp.recv()
-            data.append(recv_unpacked_data)
-
-    up.join()
+    def set_entry(self, poly_d, plain_b):
+        self.Poly_Degree = poly_d
+        self.Plain_Modulus = plain_b
     
-    return data
+    def launch_pack(self):
+        time.sleep(0.01)
+        subprocess.run([pack_prog])
+
+    def launch_unpack(self):
+        time.sleep(0.01)
+        subprocess.run([unpack_prog])    
+
+    def pack_main(self, data):
+        packed_data = []
+
+        p = multiprocessing.Process(target=self.launch_pack)
+        p.start()
+
+        with tcs.tcp_server(HOST, PORT) as tcsp:
+            # send poly_degree and plain_modulus(bits)
+            tcsp.send(self.Poly_Degree)
+            tcsp.send(self.Plain_Modulus)
+            _, get_plain_modulus = tcsp.recv()
+            self.Plain_Modulus_Specify = int(get_plain_modulus)
+
+            # send the number of data
+            tcsp.send(len(data))
+            for i in range(len(data)):
+                tcsp.send(f"{data[i]}")
+
+            # receive poly_size and packed ring coefficient
+            _, k = tcsp.recv()
+            for i in range(k):
+                _, recv_data = tcsp.recv()
+                packed_data.append(int(recv_data))
+
+        p.join()
+
+        return packed_data
+
+    def unpack_main(self, packed_data, datalen):
+        data = []
+
+        up = multiprocessing.Process(target=self.launch_unpack)
+        up.start()
+
+        with tcs.tcp_server(HOST, PORT) as tcsp:
+            # send poly_degree and plain_modulus(bits)
+            tcsp.send(self.Poly_Degree)
+            tcsp.send(self.Plain_Modulus)
+
+            # send the number of data
+            k = datalen
+            tcsp.send(k)
+
+            # send the number of packed_data
+            for i in range(len(packed_data)):
+                tcsp.send(f"{packed_data[i]}")
+
+            # receive data
+            recv_unpacked_data = -1
+            for i in range(k):
+                _, recv_unpacked_data = tcsp.recv()
+                data.append(recv_unpacked_data)
+
+        up.join()
+        
+        return data
