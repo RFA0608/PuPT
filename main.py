ab18473a764596cd0c80f6ed3bdab684a98e41b7
RFA0608 <159212138+RFA0608@users.noreply.github.com>
2025-10-16 05:24:14 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ import PnUP as pnup
 
 def main():
     # example
-    data = [3, 4, 5]
-    datalen = 3
+    pup = pnup.pupt(2**13, 24)
 
-    pack_data = pnup.pack_main(data)
+    data = [3, 4, 5, 6, 7, 8]
+    datalen = 6
+
+    pack_data = pup.pack_main(data)
     
-    recv_data = pnup.unpack_main(pack_data, datalen)
+    recv_data = pup.unpack_main(pack_data, datalen)
+
+    print(f"{pup.Plain_Modulus_Specify}")
 
     for i in range(datalen):
         print(f"{recv_data[i]}")
