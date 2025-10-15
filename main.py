e0a36291744cfbc3d6553dca1910b112c995c725
RFA0608 <159212138+RFA0608@users.noreply.github.com>
2025-10-16 04:52:19 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,92 +1,18 @@
-import time
-import multiprocessing
-import subprocess
 import sys
 sys.path.append(r"py")
+import PnUP as pnup
 
-pack_prog = './cpp/pack'
-unpack_prog = './cpp/unpack'
-
-import tcp_protocol_server as tcs
-
-HOST = 'localhost'
-PORT = 9999
-
-Poly_Degree = 2**13
-Plain_Modulus = 24
-
-def launch_pack():
-    time.sleep(0.01)
-    subprocess.run([pack_prog])
-
-def launch_unpack():
-    time.sleep(0.01)
-    subprocess.run([unpack_prog])    
-
-def pack_main(data):
-    packed_data = []
-
-    p = multiprocessing.Process(target=launch_pack)
-    p.start()
-
-    with tcs.tcp_server(HOST, PORT) as tcsp:
-        # send poly_degree and plain_modulus(bits)
-        tcsp.send(Poly_Degree)
-        tcsp.send(Plain_Modulus)
-
-        # send the number of data
-        tcsp.send(len(data))
-        for i in range(len(data)):
-            tcsp.send(f"{data[i]}")
-
-        # receive poly_size and packed ring coefficient
-        _, k = tcsp.recv()
-        for i in range(k):
-            _, recv_data = tcsp.recv()
-            packed_data.append(int(recv_data))
-
-    p.join()
-
-    return packed_data
-
-def unpack_main(packed_data, datalen):
-    data = []
-
-    up = multiprocessing.Process(target=launch_unpack)
-    up.start()
-
-    with tcs.tcp_server(HOST, PORT) as tcsp:
-        # send poly_degree and plain_modulus(bits)
-        tcsp.send(Poly_Degree)
-        tcsp.send(Plain_Modulus)
-
-        # send the number of data
-        k = datalen
-        tcsp.send(k)
-
-        # send the number of packed_data
-        for i in range(len(packed_data)):
-            tcsp.send(f"{packed_data[i]}")
-
-        # receive data
-        recv_unpacked_data = -1
-        for i in range(k):
-            _, recv_unpacked_data = tcsp.recv()
-            data.append(recv_unpacked_data)
-
-    up.join()
-    
-    return data
-
-if __name__ == "__main__":
+def main():
     # example
     data = [3, 4, 5]
     datalen = 3
 
-    pack_data = pack_main(data)
+    pack_data = pnup.pack_main(data)
     
-    recv_data = unpack_main(pack_data, datalen)
-    
+    recv_data = pnup.unpack_main(pack_data, datalen)
 
     for i in range(datalen):
         print(f"{recv_data[i]}")
+
+if __name__ == "__main__":
+    main()
